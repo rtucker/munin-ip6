7775eb8057f172dcd31c2f30288e68100763f301
Ryan Tucker <rtucker@gmail.com>
2011-05-29 12:36:58 -0400

    bug fixes, support pre-upstart systems

--- ip6_init_filters.bash
+++ ip6_init_filters.bash
@@ -12,26 +12,34 @@ MUNIN_IP6_LOCATION="${0%/*}/ip6_"  # default: current dir
 ADDRESSES=`ip -6 addr list | grep global | awk '{print $2}' | cut -d'/' -f1`
 DIDSOMETHING=""
 
-for addr in "${ADDRESSES}"
+for addr in ${ADDRESSES}
 do
-    [ -z "$DEBUG" ] || echo "$0: found address $addr"
+    [ -z "$DEBUG" ] || echo "${0##*/}: found address $addr"
     if [ -z "`ip6tables -L INPUT -v -n -x | grep -m1 $addr`" ]
     then
-        [ -z "$DEBUG" ] || echo "$0: adding input rule for $addr"
+        [ -z "$DEBUG" ] || echo "${0##*/}: adding input rule for $addr"
         ip6tables -A INPUT -d $addr
     fi
     if [ -z "`ip6tables -L OUTPUT -v -n -x | grep -m1 $addr`" ]
     then
-        [ -z "$DEBUG" ] || echo "$0: adding output rule for $addr"
+        [ -z "$DEBUG" ] || echo "${0##*/}: adding output rule for $addr"
         ip6tables -A OUTPUT -s $addr
     fi
     filename="${MUNIN_PLUGIN_DIR}/ip6_`echo $addr | sed 's/:/_/g'`"
     if [ ! -x "${filename}" ]
     then
-        [ -z "$DEBUG" ] || echo "$0: linking $MUNIN_IP6_LOCATION -> $filename"
+        [ -z "$DEBUG" ] || echo "${0##*/}: linking $MUNIN_IP6_LOCATION -> $filename"
         ln -s "$MUNIN_IP6_LOCATION" "$filename"
         DIDSOMETHING="yup"
     fi
 done
 
-[ -z "${DIDSOMETHING}" ] || service munin-node restart
+if [ -n "${DIDSOMETHING}" ]
+then
+    if [ -x "/usr/sbin/service" ]
+    then
+        /usr/sbin/service munin-node restart
+    else
+        /etc/init.d/munin-node restart
+    fi
+fi
